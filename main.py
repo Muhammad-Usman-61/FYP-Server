7d9ff55b57aeea9c27fe0cf0612d5010c2e363e0
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-05-11 03:43:12 +0500

    main.py updated

--- main.py
+++ main.py
@@ -6,5 +6,10 @@ app = Flask(__name__)
 def home():
     return "This is the server for FYP Project, Send request on /data endpoint to get result"
 
+@app.route('/data', methods=['POST', 'GET'])
+def data():
+    return "This is the server for FYP Project, Send request on /data endpoint to get result"
+
+
 if __name__ == '__main__':
     app.run(debug=True)

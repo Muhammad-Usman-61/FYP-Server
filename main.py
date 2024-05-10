c8dc236bd30a6198efa7ccfbf304f5c7b2950b55
Muhammad-Usman-61 <muhammad196usman@gmail.com>
2024-05-11 03:42:24 +0500

    main.py updated

--- main.py
+++ main.py
@@ -1,21 +1,10 @@
-from flask import Flask, request
-from flask_cors import CORS
-from keras.models import load_model
-from keras.preprocessing import image
-import numpy as np
-import io
-
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "This is the server for FYP Project, Send request on /data endpoint to get result"
-
-@app.route('/data', methods=['POST', 'GET'])
-def data():
-    return "This is the server for FYP Project, Send request on /data endpoint to get result"
-
 
 if __name__ == '__main__':
     app.run(debug=True)
